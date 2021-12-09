419fa19c89ffa6b98f6ea036796ce4425029e03e
carr <carrlu@163.com>
2021-12-09 16:36:37 +0800

    init

---
+++ src/components/Loading.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="loading" v-if="show">加载中...</div>
+</template>
+
+<script>
+export default {
+    props: {
+        show: {
+            type: Boolean,
+            default: true
+        }
+    }
+}
+</script>
